e41753d1cb4cf761d241b676a7ea2de975e448dc
Bri McOmber <bri.mcomber@gmail.com>
2020-11-05 18:02:45 -0700

    Added style to category header buttons

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,16 +1,25 @@
 function showCategory(category) {
+    const side_menu = document.querySelector('#side-menu')
 
-    const container = document.querySelector('#side-menu')
+    document.querySelectorAll('.selected').forEach(btn => {
+        btn.classList.remove('selected')
+    })
+
     if (category == 'none') {
-        container.style.display = 'none'
+        side_menu.style.display = 'none'
     } else {
-        container.style.display = 'inline'
-        for (let child of container.children) {
+        side_menu.style.display = 'inline'
+        for (let child of side_menu.children) {
             if (child.id == category) {
                 child.style.display = 'block'
             }
             else {
                 child.style.display = 'none'
+            }
+        }
+        for (let child of document.querySelector('#category-header').children) {
+            if (child.innerHTML.toLowerCase() == category) {
+                child.classList.add('selected')
             }
         }
     }
