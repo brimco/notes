a0bbe67fd7798c54bb06e2b2686737948d91538d
Bri McOmber <bri.mcomber@gmail.com>
2020-11-07 11:48:13 -0700

    Added color picker

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -1,3 +1,8 @@
+// color picker
+document.querySelector('#pick-color').addEventListener('change', (event) => {
+    document.querySelector('#display-color-picker').innerHTML = event.target.value
+})
+
 // hex colors
 const choose = document.querySelector('#choose-color')
 const display = document.querySelector('#display-color')
