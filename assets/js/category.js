f4261d243d8f1e4b72b7140b204709ec2ffff31e
Bri McOmber <bri.mcomber@gmail.com>
2020-11-07 13:30:38 -0700

    Added styling to sidebar

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,26 +1,54 @@
 function showCategory(category) {
     const side_menu = document.querySelector('#side-menu')
 
+    // unselect all categories
     document.querySelectorAll('.selected').forEach(btn => {
         btn.classList.remove('selected')
     })
 
     if (category == 'none') {
-        side_menu.style.display = 'none'
+        hideSidebar()
     } else {
-        side_menu.style.display = 'inline'
+        showSidebar()
+
+        // show links for selected category only
         for (let child of side_menu.children) {
             if (child.id == category) {
                 child.style.display = 'block'
-            }
-            else {
+            } else if (child.id != 'close-sidebar') {
                 child.style.display = 'none'
             }
         }
+
+        // select the right category
         for (let child of document.querySelector('#category-header').children) {
             if (child.innerHTML.toLowerCase() == category) {
                 child.classList.add('selected')
             }
         }
+    }
+}
+
+function hideSidebar() {
+    const side_menu = document.querySelector('#side-menu')
+    side_menu.style.width = '0'
+    side_menu.style.padding = '0'
+}
+
+function showSidebar() {
+    const side_menu = document.querySelector('#side-menu')
+    side_menu.style.width = '250px'
+    side_menu.style.padding = '1em'
+}
+
+function toggleSidebar() {
+    console.log('toggle')
+    const side_menu = document.querySelector('#side-menu')
+    if (side_menu.style.width == '250px') {
+        console.log('hide')
+        hideSidebar()
+    } else {
+        console.log('show')
+        showSidebar()
     }
 }
